aed46981c4e6c335688bbc16d493e6ae46989b4a
uzochi-dimkpa <uzochidimkpa@gmail.com>
2023-10-13 12:42:25 -0400

    convolution-fix-3

--- performance-modeling/convolution.sh
+++ performance-modeling/convolution.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-sbatch --partition=Centaurus --chdir=`pwd` --time=01:00:00 --nodes=1 --ntasks-per-node=8 --job-name=convl run convol.sh
-sbatch --partition=Centaurus --chdir=`pwd` --time=01:00:00 --nodes=1 --ntasks-per-node=8 --job-name=convl_par run convol_par.sh
+sbatch --partition=Centaurus --chdir=`pwd` --time=01:00:00 --nodes=1 --ntasks-per-node=8 --error='convol_err.txt' --job-name=convl convol.sh
+sbatch --partition=Centaurus --chdir=`pwd` --time=01:00:00 --nodes=1 --ntasks-per-node=8 --error='convol_par_err.txt' --job-name=convl_par convol_par.sh
