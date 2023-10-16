ebe0b35422a9856c021c91e1e415c3b336506c58
uzochi-dimkpa <uzochidimkpa@gmail.com>
2023-10-16 16:44:59 -0400

    convolution-fix-8

--- performance-modeling/convolution.sh
+++ performance-modeling/convolution.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 sbatch --partition=Centaurus --chdir=`pwd` --time=24:00:00 --nodes=1 --ntasks-per-node=16 --mem=120G --mail-type=END,FAIL,REQUEUE,TIME_LIMIT --mail-user=udimkpa@uncc.edu --job-name=convl convol.sh
-sbatch --partition=Centaurus --chdir=`pwd` --time=24:00:00 --nodes=1 --ntasks-per-node=16 --mem=120G --mail-type=END,FAIL,REQUEUE,TIME_LIMIT --mail-user=udimkpa@uncc.edu --job-name=convl_par convol_par.sh
+sbatch --partition=Centaurus --chdir=`pwd` --time=24:00:00 --nodes=1 --ntasks-per-node=16 --mem=120G --mail-type=END,FAIL,REQUEUE,TIME_LIMIT --mail-user=udimkpa@uncc.edu --job-name=convlpar convol_par.sh
